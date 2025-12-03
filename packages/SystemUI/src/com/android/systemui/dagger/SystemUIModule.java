36e2a44d7ab01ca88dda5a62dc18f6defcd93620
Amaan Qureshi <git@amaanq.com>
2025-12-03 13:22:28 -0500

    feat: usb-tether-tile
    Add USB Tethering QS tile for quick toggle of USB internet sharing.
    Shows unavailable state when no USB device is connected.

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -34,6 +34,7 @@ import com.android.systemui.BootCompleteCacheImpl;
 import com.android.systemui.CameraProtectionModule;
 import com.android.systemui.qs.tiles.CaffeineTileModule;
 import com.android.systemui.qs.tiles.ReadingModeTileModule;
+import com.android.systemui.qs.tiles.UsbTetherTileModule;
 import com.android.systemui.CoreStartable;
 import com.android.systemui.KairosCoreStartableModule;
 import com.android.systemui.SystemUISecondaryUserService;
@@ -299,6 +300,7 @@ import javax.inject.Named;
         TopUiModule.class,
         TouchpadModule.class,
         TunerModule.class,
+        UsbTetherTileModule.class,
         UserDomainLayerModule.class,
         UserModule.class,
         UtilModule.class,
